44286feb31e9ba5dbc44ca3e9d940397db4082ed
coryrobertson <cory.robertson17@gmail.com>
2024-01-12 19:23:24 -0800

    Implemented auto updating, most likely won't work on the first try but that's alright :)

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use chrono::{Days, NaiveDate};
+use chrono::{DateTime, Days, Local, NaiveDate};
 use cr_project_planner::project::{Project, ProjectDisplayAction};
 use eframe::{Frame, Storage};
 use egui::scroll_area::ScrollBarVisibility;
@@ -6,6 +6,11 @@ use egui::style::{ScrollStyle, Spacing};
 use egui::{Color32, Context, Layout, ScrollArea, Style, ViewportCommand};
 use serde::{Deserialize, Serialize};
 use std::ops::Not;
+use std::thread;
+use std::thread::JoinHandle;
+use self_update::cargo_crate_version;
+use self_update::errors::Error;
+use self_update::update::{Release, UpdateStatus};
 
 #[derive(Serialize, Deserialize)]
 #[serde(default)]
@@ -28,6 +33,22 @@ pub struct ProjectPlanner {
 
     #[serde(skip)]
     add_project_open: bool,
+
+    last_open: DateTime<Local>,
+
+    #[serde(skip)]
+    first_run: bool,
+
+    #[serde(skip)]
+    update_available: Option<Release>,
+
+    auto_update_seen_version: Option<String>,
+    
+    #[serde(skip)]
+    update_thread: Option<JoinHandle<Result<UpdateStatus, Error>>>,
+
+    #[serde(skip)]
+    auto_update_status: Option<AutoUpdateStatus>,
 }
 
 impl ProjectPlanner {
@@ -46,20 +67,84 @@ impl Default for ProjectPlanner {
             projects: vec![],
             project_name_selected: "DEFAULT PROJECT NAME".to_string(),
             project_description_selected: "".to_string(),
-            start_date_selected: chrono::offset::Local::now().date_naive(),
-            end_date_selected: chrono::offset::Local::now()
+            start_date_selected: Local::now().date_naive(),
+            end_date_selected: Local::now()
                 .checked_add_days(Days::new(1))
                 .unwrap_or_default()
                 .date_naive(),
             editing_project: false,
             editing_project_index: 0,
             add_project_open: false,
+            last_open: Local::now(),
+            first_run: true,
+            update_available: None,
+            auto_update_seen_version: None,
+            update_thread: None,
+            auto_update_status: None,
         }
     }
 }
 
+fn get_release_list() -> Result<Vec<Release>, Box<dyn std::error::Error>> {
+    let list = self_update::backends::github::ReleaseList::configure()
+        .repo_owner("CoryRobertson")
+        .repo_name("cr_project_planner")
+        .build()?
+        .fetch()?;
+
+    Ok(list)
+}
+
+fn update_program() -> JoinHandle<Result<UpdateStatus, Error>> {
+    thread::spawn(|| {
+        match self_update::backends::github::UpdateBuilder::new()
+            .repo_owner("CoryRobertson")
+            .repo_name("cr_project_planner")
+            .show_download_progress(true)
+            .no_confirm(true)
+            .current_version(cargo_crate_version!())
+            .build() {
+            Ok(updater) => {
+                match updater.update_extended() {
+                    Ok(status) => {
+                        Ok(status)
+                    }
+                    Err(err) => {
+                        Err(err)
+                    }
+                }
+            }
+            Err(err) => {
+                Err(err)
+            }
+        }
+    })
+}
+
+
+
 impl eframe::App for ProjectPlanner {
     fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+
+        if self.first_run {
+            self.first_run = false;
+            if self.last_open.signed_duration_since(Local::now()).num_hours() > 1 {
+                match get_release_list() {
+                    Ok(list) => {
+                        if let Some(release) = list.first() {
+                            if let Ok(greater_bump) = self_update::version::bump_is_greater(cargo_crate_version!(),&release.version) {
+                                if greater_bump {
+                                    self.update_available = Some(release.clone());
+                                }
+                            }
+                        }
+                    }
+                    Err(_) => {}
+                }
+            }
+
+        }
+
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.set_style(Style {
                 spacing: Spacing {
@@ -182,6 +267,96 @@ impl eframe::App for ProjectPlanner {
                 }
             });
         }
+
+        if let Some(update) = self.update_available.clone() {
+            let show_update_screen = match &self.auto_update_seen_version {
+                None => {
+                    true
+                }
+                Some(seen_version) => {
+                    self_update::version::bump_is_greater(&update.version,&seen_version).unwrap_or(false)
+                }
+            };
+
+            let update_done = self.auto_update_status.is_some();
+            
+            if show_update_screen || update_done {
+                egui::Window::new("Update Available").show(ctx,|ui| {
+                    if !update_done {
+                        ui.heading("There is a program update available.");
+                        ui.label(format!("Current version: {}", cargo_crate_version!()));
+                        ui.label(format!("Updated version version: {}", update.version));
+                        let finished = match &self.update_thread {
+                            None => {
+                                if ui.button("Automagically update").clicked() {
+                                    self.update_thread = Some(update_program());
+                                }
+                                if ui.button("Skip this version").clicked() {
+                                    self.auto_update_seen_version = Some(update.version.clone());
+                                    self.update_available = None;
+                                }
+                                false
+                            }
+                            Some(update_thread) => {
+                                if !update_thread.is_finished() {
+                                    ui.horizontal(|ui| {
+                                        ui.label("Updating...");
+                                        ui.spinner();
+                                    });
+                                    false
+                                }
+                                else {
+                                    true
+                                }
+                            }
+                        };
+
+                        if finished {
+                            if let Some(thread) = self.update_thread.take() {
+                                self.auto_update_status = Some(thread.join().map(|result| {
+                                    match result {
+                                        Ok(update) => {
+                                            match update {
+                                                UpdateStatus::UpToDate => {
+                                                    AutoUpdateStatus::UpToDate
+                                                }
+                                                UpdateStatus::Updated(ver) => {
+                                                    AutoUpdateStatus::Updated(ver.version)
+                                                }
+                                            }
+                                        }
+                                        Err(_) => {
+                                            AutoUpdateStatus::Error
+                                        }
+                                    }
+                                }).unwrap_or(AutoUpdateStatus::Error));
+                            }
+                        }
+                    } else {
+                        if let Some(status) = self.auto_update_status.as_ref() {
+                            match status {
+                                AutoUpdateStatus::Error => {
+                                    ui.label("An error occurred while updating");
+                                    // TODO: give user more recourse to fix this, or least tell them maybe why it happened ?
+                                }
+                                AutoUpdateStatus::UpToDate => {
+                                    ui.label("You were already on the latest version, whoops!");
+                                }
+                                AutoUpdateStatus::Updated(_) => {
+                                    ui.label("Update finished");
+                                    ui.label("Simply close and re-open the program to complete the update.");
+                                }
+                            }
+                            if ui.button("Close this window").clicked() {
+                                self.auto_update_status = None;
+                            }
+                        }
+
+                    }
+                });
+            }
+            
+        }
     }
 
     fn save(&mut self, storage: &mut dyn Storage) {
@@ -191,4 +366,10 @@ impl eframe::App for ProjectPlanner {
     fn persist_egui_memory(&self) -> bool {
         true
     }
+}
+
+enum AutoUpdateStatus {
+    Error,
+    UpToDate,
+    Updated(String),
 }
